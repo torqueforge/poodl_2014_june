2dbce99062a49339b39c1e140b8ce5acde2265c5
Raul Gracia <raulgracialario@gmail.com>
2014-06-26 14:17:45 +0100

    WIP Factory implementation

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,23 +1,45 @@
 class Bottles
 
-  def verse(number)
-    "#{quantity(number)} #{container(number)} of #{content} on the wall, " +
-    "#{quantity(number).downcase} #{container(number)} of #{content}.\n" +
-    "#{action(number)}, " +
-    "#{quantity(next_number(number)).downcase} #{container(next_number(number))} of #{content} on the wall.\n"
-  end
-
-  def verses(upper_bound, lower_bound)
-    upper_bound.downto(lower_bound).collect {|verse_number| verse(verse_number) }.join("\n")
-  end
-
   def song
     verses(99, 0)
   end
 
-  private
+  def verses(upper_bound, lower_bound)
+    upper_bound
+    .downto(lower_bound)
+    .collect {|verse_number| verse(verse_number) }
+    .join("\n")
+  end
 
-  def container(number)
+  def verse(number)
+    current_bottle = to_bottle_number(number)
+    next_bottle = to_bottle_number(number == 0 ? 99 : number - 1)
+
+    "#{current_bottle.quantity} #{current_bottle.container} of beer on the wall, " +
+    "#{current_bottle.quantity.downcase} #{current_bottle.container} of beer.\n" +
+    "#{current_bottle.action}, " +
+    "#{next_bottle.quantity.downcase} #{next_bottle.container} of beer on the wall.\n"
+  end
+
+  def to_bottle_number(number)
+    if number == 0
+      BottleNumber0.new(number)
+    else
+      BottleNumber.new(number)
+    end
+  end
+
+end
+
+
+class BottleNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def container
     if number == 1
       "bottle"
     else
@@ -25,40 +47,31 @@ class Bottles
     end
   end
 
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
+  def action
+    "Take #{pronoun} down and pass it around"
   end
 
-  def pronoun(number)
+  def quantity
+    "#{number}"
+  end
+
+  private
+
+  def pronoun
     if number == 1
       "it"
     else
       "one"
     end
   end
+end
 
-  def quantity(number)
-    if number == 0
-      "No more"
-    else
-      "#{number}"
-    end
+class BottleNumber0 < BottleNumber
+  def quantity
+    "No more"
   end
 
-  def next_number(number)
-    if number == 0
-      99
-    else
-      number - 1
-    end
+  def action
+    "Go to the store and buy some more"
   end
-
-  def content
-    "beer"
-  end
-
 end
