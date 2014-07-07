0c89087cad00f9b9d122ffef10e241c817238cdd
Matt Wynne <matt@mattwynne.net>
2014-07-07 21:31:00 +0100

    Extreme composition: a study
    I was curious to try this after the second POODL course. I unwound the
    inheritance hierarchy, pushing the conditionals back into the factory
    method, and creating different types for each branch of the conditional.
    
    Two patterns emerged: that the Container needs to switch on 1..n
    bottles, and that the rest of them (description, action, next number)
    all switch on 0..n bottles.
    
    So I think we could have two entirely different BottleNumber classes for
    the 0/n cases, composed of a container for the 1/n cases.

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -13,65 +13,104 @@ class Bottles
 
   def verse(number)
     current_bottle = to_bottle_number(number)
-    next_bottle = to_bottle_number(number == 0 ? 99 : number - 1)
+    next_bottle = to_bottle_number(current_bottle.next_number)
 
-    "#{current_bottle.quantity} #{current_bottle.container} of beer on the wall, " +
-    "#{current_bottle.quantity.downcase} #{current_bottle.container} of beer.\n" +
+    "#{current_bottle.to_s.capitalize} #{current_bottle.container} of beer on the wall, " +
+    "#{current_bottle} #{current_bottle.container} of beer.\n" +
     "#{current_bottle.action}, " +
-    "#{next_bottle.quantity.downcase} #{next_bottle.container} of beer on the wall.\n"
+    "#{next_bottle} #{next_bottle.container} of beer on the wall.\n"
   end
 
   def to_bottle_number(number)
+    container = Container.for(number)
+
     if number == 0
-      BottleNumber0.new(number)
+      BottleNumber.new(Action::GoToTheStore.new, container, Description::NoMore.new, 99)
     else
-      BottleNumber.new(number)
+      BottleNumber.new(Action::TakeOneDown.new(container), container, Description::Default.new(number), number - 1)
     end
   end
 
 end
 
-
-class BottleNumber
-  attr_reader :number
-
-  def initialize(number)
-    @number = number
+module Container
+  def self.for(number)
+    return Singular.new if number == 1
+    Plural.new
   end
 
-  def container
-    if number == 1
+  class Singular
+    def pronoun
+      "it"
+    end
+
+    def to_s
       "bottle"
-    else
-      "bottles"
     end
   end
 
-  def action
-    "Take #{pronoun} down and pass it around"
-  end
+  class Plural
+    def to_s
+      "bottles"
+    end
 
-  def quantity
-    "#{number}"
-  end
-
-  private
-
-  def pronoun
-    if number == 1
-      "it"
-    else
+    def pronoun
       "one"
     end
   end
 end
 
-class BottleNumber0 < BottleNumber
-  def quantity
-    "No more"
+module Action
+  class TakeOneDown
+    attr_reader :container
+
+    def initialize(container)
+      @container = container
+    end
+
+    def to_s
+      "Take #{container.pronoun} down and pass it around"
+    end
   end
 
-  def action
-    "Go to the store and buy some more"
+  class GoToTheStore
+    def to_s
+      "Go to the store and buy some more"
+    end
+  end
+end
+
+module Description
+  class Default
+    attr_reader :number
+
+    def initialize(number)
+      @number = number
+    end
+
+    def to_s
+      number.to_s
+    end
+  end
+
+  class NoMore
+    def to_s
+      "no more"
+    end
+  end
+end
+
+class BottleNumber
+  attr_reader :action, :container, :description, :next_number
+
+  def initialize(action, container, description, next_number)
+    @action = action
+    @container = container
+    @description = description
+    @next_number = next_number
+  end
+
+  def to_s
+    description.to_s
   end
 end
